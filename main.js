570d615261aa3332983d80c117ae8fc5c087cb19
Random-Person100 <91764794+Random-Person100@users.noreply.github.com>
2022-04-07 18:02:02 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -3,17 +3,25 @@ function preload(){
 }
 function setup(){
     canvas=createCanvas(640,480)
-    canvas.position(110,250)
+    canvas.position(350,200)
     video=createCapture(VIDEO)
     video.hide()
     tint_color=""
 }
 function draw(){
-image(video,0,0,640,480)
-tint(tint_color)
-}
-function filter_tint(){
-    tint_color=document.getElementById("color_name").value
+image(video,100,50,400,300)
+fill("blue")
+stroke("black")
+circle(50,50,80)
+circle(50,430,80)
+circle(590,50,80)
+circle(590,430,80)
+fill("red")
+stroke("red")
+rect(90,30,460,20)
+rect(90,450,460,20)
+rect(40,90,20,300)
+rect(580,90,20,300)
 }
 function take_snapshot(){
     save('selfie.png')
